294c154df23a2a39bba54784f6e94c17548397ca
Patlolla-Harshitha <161221584+Patlolla-Harshitha@users.noreply.github.com>
2025-05-24 21:56:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Collapse functionality
+const collapses = document.querySelectorAll('.collapse');
+const contents = document.querySelectorAll('.collapse-content');
+
+collapses.forEach((collapse, index) => {
+  collapse.addEventListener('click', () => {
+    collaps
+::contentReference[oaicite:9]{index=9}
+ 
